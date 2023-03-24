50b9773a99f308b8c5aca2907880947a18dc8ea3
Minh Tô <milkissily@gmail.com>
2023-03-25 02:13:48 +0700

    new1

--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -11,17 +11,48 @@ class UserController {
             })
             .catch(next);
     }
+
     async detailUser(req, res, next) {
-        console.log('id user: ', req.params._id);
+        const id = req.params._id;
         try {
-            const user = await User.findById(req.params._id).exec();
-            console.log(user);
-            res.render('detail-user', { layout: 'home', user:getSingleObject(user) });
+            const user = await User.findById(id).exec();
+            console.log(getSingleObject(user));
+            res.render('detail-user', { layout: 'home', user: getSingleObject(user) });
         } catch (err) {
             console.error(err);
             res.send(err);
         }
     }
+
+    updateUser(req, res, next) {
+        const formData = req.body;
+        if (req.file !== undefined && req.file !== null) {
+            formData.image = `/image/${req.file.originalname}`;
+        } else {
+            console.log('nullsssssssssssss')
+            formData.image = formData.old;
+        }
+        delete formData.old;
+        User.updateOne({ _id: req.params._id }, formData)
+            .then(() => res.redirect('/users'))
+            .catch(next);
+    }
+
+    deleteUser(req, res, next) {
+        User.deleteOne({ _id: req.params._id })
+            .then(() => res.redirect('/users'))
+            .catch(next);
+    }
+
+    async searchUser(req, res, next) {
+        const query = req.body.email.toString();
+        await User.findOne({ email: query })
+            .then(users => {
+                console.log(getSingleObject(users));
+                res.render('users', { layout: 'home', users: getSingleObject(users) });
+            }).catch(next);
+    }
+
 }
 
 module.exports = new UserController;
